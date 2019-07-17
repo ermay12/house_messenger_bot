000636c0b88d7933d49fcae94fbef5f5da7990a4
EC2 Default User <ec2-user@ip-172-31-27-137.us-east-2.compute.internal>
2019-07-17 05:15:12 +0000

    added http server

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const login = require("facebook-chat-api");
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.send('HEY!')
+});
+app.listen(3000, () => console.log('Server running on port 3000'));
 
 // Create simple echo bot
 login({email: "sake.jrhouse", password: "sakejr"}, (err, api) => {
